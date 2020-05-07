d254e4baec613147b688070f9dac8367da413705
DreamLi <18829346477@163.com>
2020-05-07 14:10:10 +0800

    init project

---
+++ src/main/java/club/javafamily/avatardata/weather/web/config/WeatherConfiguration.java
@@ -0,0 +1,9 @@
+package club.javafamily.avatardata.weather.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WeatherConfiguration {
+
+}
---
+++ src/test/java/club/javafamily/avatardata/weather/web/JavafamilyAvatardataWeatherWebApplicationTests.java
@@ -0,0 +1,11 @@
+package club.javafamily.avatardata.weather.web;
+
+import org.junit.jupiter.api.Test;
+
+class JavafamilyAvatardataWeatherWebApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
